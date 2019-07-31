4ad468c14bc6b7104e28ecd3b221d362f0be74e9
AyumuSaito <51943501+AyumuSaito@users.noreply.github.com>
2019-07-31 12:04:05 +0900

    Update median_of_median.c

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -12,48 +12,50 @@ void swap(int *p, int *q){
   *q = tmp;
 }
 
-int median(int A[], int n, int m){
-    int B[N];
-    int i, j, r;
-    if(n <= m){
-        for(j=0;j<n/2+1;j++){
-            for(i=j+1; i < n; i++){
-                if(A[i]<A[j]) swap(A+i, A+j);
-            }
+void bubble_sort(int A[], int n){
+    int i, j;
+    for(j=0;j<n/2+1;j++){
+        for(i=j+1; i < n; i++){
+            if(A[i]<A[j]) swap(A+i, A+j);
         }
-        return A[n/2];
-    }
-    
-    else{
-        for(i=0; i < n/5; i++){
-            B[i] = median(A+(5*i),5,5);
-        }
-        B[n/5] = median(A+(5*(n/5)),(n+4)%5+1,5);
-        r = (n+4)/5;
-        return median(B,r,r);
     }
 }
 
 int quick_select(int A[], int n, int k){
-  int i, j, m, p, pivot;
-    m = median(A,n,5);
-    for(p=0;p<n;p++){
-        if(A[p] == m){
-            pivot = A[p];
-            break;
+    int i, j, m, p, pivot;
+    int B[N];
+    if(n <= 5){
+        for(j=0; j<n; j++){
+            for(i=j+1; i<n; i++) if(A[j]>A[i]) swap(A+i,A+j);
         }
+        return A[k];
     }
-  A[p] = A[0];
-  A[0] = pivot;
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      swap(A+i, A+j);
-      j++;
+    else{
+        for(i=0; i<n/5; i++){
+            bubble_sort(A+(5*i),5);
+            B[i] = A[5*i+2];
+        }
+        bubble_sort(A+n/5*5,(n+4)%5+1);
+        B[n/5] = A[n/5*5+((n+4)%5+1)/2];
+        m = quick_select(B,n/5+1,(n/5+1)/2);
+        for(p=0; p<n; p++){
+            if(A[p] == m){
+                pivot = A[p];
+                break;
+            }
+        }
+        A[p] = A[0];
+        A[0] = pivot;
+        for(i = j = 1; i < n; i++){
+            if(A[i] <= pivot){
+                swap(A+i, A+j);
+                j++;
+            }
+        }
+        if(j == k+1) return pivot;
+        else if(j < k+1) return quick_select(A+j, n-j, k-j);
+        else return quick_select(A+1, j-1, k);
     }
-  }
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
 }
 
 
